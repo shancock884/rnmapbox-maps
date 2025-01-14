8db922714faa7cbf912e05a20f00052167289bab
Simon Hancock <simon@linkingdrones.com>
2025-01-14 09:47:34 +0000

    Include state it return when calling getPacks on iOS

--- ios/RNMBX/Offline/RNMBXOfflineModule.swift
+++ ios/RNMBX/Offline/RNMBXOfflineModule.swift
@@ -510,6 +510,7 @@ class RNMBXOfflineModule: RCTEventEmitter {
       
       resolve(results.map { (id, geometry_region_metadata) -> [String:Any] in
         let (geometry, region, metadata) = geometry_region_metadata
+        let prevState = self.tileRegionPacks[region.id]?.state ?? State.unknown
         let ret = self.convertRegionToJSON(region: region, geometry: geometry, metadata: metadata)
         var pack = self.tileRegionPacks[region.id] ?? TileRegionPack(
           name: region.id,
@@ -529,7 +530,7 @@ class RNMBXOfflineModule: RCTEventEmitter {
     }
   }
   
-  func convertRegionToJSON(region: TileRegion, geometry: Geometry, metadata: [String:Any]?) -> [String:Any] {
+  func convertRegionToJSON(region: TileRegion, geometry: Geometry, metadata: [String:Any]?, state: State) -> [String:Any] {
     let bb = RNMBXFeatureUtils.boundingBox(geometry: geometry)
     
     if let bb = bb {
@@ -547,7 +548,7 @@ class RNMBXOfflineModule: RCTEventEmitter {
         "requiredResourceCount": region.requiredResourceCount,
         "completedResourceCount": region.completedResourceCount,
         "completedResourceSize": region.completedResourceSize,
-        "state": completed ? State.complete.rawValue : State.unknown.rawValue,
+        "state": completed ? State.complete.rawValue : state.rawValue,
         "metadata": String(data:try! JSONSerialization.data(withJSONObject: metadata, options: [.prettyPrinted]), encoding: .utf8),
         "bounds": jsonBounds
       ]
